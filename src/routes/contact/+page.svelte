a18ca2473f9bfbbd21806aaf8a12b49bebb3c210
Romeo Beaurain <beaurainr@Romeos-MacBook-Air.local>
2024-05-22 15:21:07 -0500

    email sys

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,27 +1,71 @@
 <script>
-	import NavBar from '../../lib/components/navBar.svelte';
+	import NavBar from "../../lib/components/navBar.svelte";
 
-    /** @type {import('./$types').PageData} */
-    export let data;
+    export let form;
 </script>
 
-<div>
+<div class="wrapper">
     <NavBar></NavBar>
-    <div class="container">
-        <div class="form">
-            <div class="email">
-                <form action="beaurainromeo@gmail.com" method="post" enctype="text/plain">
-                    FirstName: <input type="text" name="FirstName">
-                    Email: <input type="text" name="Email">
-                    <input type="submit" name="submit" value="Submit">
-                </form>
+    <fieldset>
+        <legend>Send Emails</legend>
+        <form method="POST" class="container">
+            <div class="input">
+                <label for="">Your email:</label>
+                <input name="to" type="email" value="" placeholder="email"/>
             </div>
-            <div class="info"></div>
-        </div>
-    </div>
-    Matte maatte
+            <div class="input">
+                <label for="">Subject:</label>
+                <input name="subject" type="text" value="" placeholder="subject"/>
+            </div>
+            <div class="input">
+                <label for="">Body:</label>
+                <textarea name="body" rows="6" value="" placeholder="body"/>
+            </div>
+            <button type="submit">Send</button>
+        </form>
+        <p class="success">{form?.success || ""}</p>
+    </fieldset>
 </div>
 
 <style>
-
+    .wrapper {
+        max-width: 800px;
+        margin: 2em auto;
+    }
+    fieldset {
+        padding: 2em;
+        box-shadow: 0 10px 15px -3px #0000001a, 0 4px 6px -4px #0000001a;
+    }
+    legend {
+        font-weight: bold;
+        font-size: 2em;
+        padding: 0 0.25em;
+        color: #333;
+    }
+    .container {
+        max-width: 400px;
+    }
+    .input {
+        display: grid;
+        margin-bottom: 1.5em;
+    }
+    label {
+        font-weight: 600;
+        margin-bottom: 0.25em;
+        color: #555;
+    }
+    input {
+        padding: 5px;
+    }
+    button {
+        cursor: pointer;
+        padding: 0.5em 1em;
+        border-radius: 4px;
+        border: 1px solid rgb(5, 151, 64);
+        background-color: rgb(5, 151, 64);
+        color: white
+    }
+    .success {
+        color: #0a5;
+    }
 </style>
